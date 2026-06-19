87c6f2bbec4cc66084fa3310279f10adb763da46
yasmin <yasminsherif2028@gmail.com>
2026-06-19 12:17:38 +0300

    Revert "Merge branch 'main' of https://github.com/yasminsherif200/TAYEH"
    This reverts commit efb3fe8406f867681cf3115c05aa4131f27b8aa3, reversing
    changes made to ec7b7b899b732d7464b929b810cbf7fea0f03033.

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
-const BASE_URL = 'https://api.tayeh.ourgrid.dev'
+const BASE_URL = 'https://tayeh.ourgrid.dev'
 
 export async function sendMessage(message, lat, lng) {
   const formData = new FormData()
   formData.append('message', message)
-  formData.append('latitude', lat)
-  formData.append('longitude', lng)
+  formData.append('lat', lat)
+  formData.append('lng', lng)
 
   const response = await fetch(`${BASE_URL}/api/search`, {
     method: 'POST',
