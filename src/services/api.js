34dc5907e034d40a4ef0ce68f5761fcb08bf71f7
Alaa Okasha <mxmmotnt1@gmail.com>
2026-06-19 11:17:00 +0300

    fix: rename body parameters

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ const BASE_URL = 'https://api.tayeh.ourgrid.dev'
 export async function sendMessage(message, lat, lng) {
   const formData = new FormData()
   formData.append('message', message)
-  formData.append('lat', lat)
-  formData.append('lng', lng)
+  formData.append('latitude', lat)
+  formData.append('longitude', lng)
 
   const response = await fetch(`${BASE_URL}/api/search`, {
     method: 'POST',
